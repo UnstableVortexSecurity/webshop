46d4119aec5446d2c9d1d30bb19cfdbd5214ee53
Torma Kristóf <tormakristof@tormakristof.eu>
2020-11-28 05:43:54 +0100

    profileview

--- src/views/profileview.py
+++ src/views/profileview.py
@@ -2,6 +2,11 @@
 from flask import render_template
 from flask_classful import FlaskView
 
+from flask_security.decorators import login_required
+from flask_security import current_user
+
+from models import Item
+
 """
 Profile VIEW
 """
@@ -14,5 +19,9 @@ __version__text__ = "1"
 
 class ProfileView(FlaskView):
 
+    @login_required
     def index(self):
-        return render_template('profile.html')
+
+        useritems = Item.query.filter_by(uploader_id=current_user.id)
+
+        return render_template('profile.html', images=useritems)
