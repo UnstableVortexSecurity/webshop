44139a420639e8f9f8b891f8a55ac44432f478ad
marcsello <punkosdmarcell@rocketmail.com>
2020-11-29 03:15:11 +0100

    Added the first test case

---
+++ src/tests/__init__.py
@@ -0,0 +1,5 @@
+import os
+os.environ['CAFF_PREVIEWER_ENDPOINT'] = ''
+os.environ['MINIO_ENDPOINT'] = ''
+os.environ['MINIO_ACCESS_KEY'] = ''
+os.environ['MINIO_SECRET_KEY'] = ''
---
+++ src/tests/test_access_allowed.py
@@ -0,0 +1,18 @@
+import pytest
+
+import app
+
+
+@pytest.fixture
+def client():
+    app.app.config['TESTING'] = True
+
+    with app.app.test_client() as client:
+        yield client
+
+
+def test_login_required(client):
+
+    r = client.get('/content/caff/1')
+
+    assert r.status_code == 302
