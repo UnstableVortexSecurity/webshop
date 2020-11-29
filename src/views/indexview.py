1dd4e5eff39a2e9b34bef60144fff73d2af2f16f
marcsello <punkosdmarcell@rocketmail.com>
2020-11-29 02:27:36 +0100

    Implemented search

--- src/views/indexview.py
+++ src/views/indexview.py
@@ -1,8 +1,10 @@
 #!/usr/bin/env python3
-from flask import render_template
+from flask import render_template, request
 from flask_classful import FlaskView
 
 from models import Item
+
+import bleach
 
 """
 Index VIEW
@@ -15,9 +17,16 @@ __version__text__ = "1"
 
 
 class IndexView(FlaskView):
-
     route_base = '/'
 
     def index(self):
-        items = Item.query.all()
-        return render_template("index.html", items=items)
+        search_query = request.args.get('search')
+
+        if search_query:
+            search_query = bleach.clean(search_query, tags=[])
+            # https://stackoverflow.com/questions/31949733/is-a-sqlalchemy-query-vulnerable-to-injection-attacks/31949750#31949750
+            items = Item.query.filter(Item.name.ilike(f"%{search_query}%")).all()
+        else:
+            items = Item.query.all()
+
+        return render_template("index.html", items=items, search_query=search_query)
